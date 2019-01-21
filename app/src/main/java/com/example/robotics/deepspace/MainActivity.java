eea5a37c2405eaceaf5858057b48f07d8a9e4b41
Connor Fogarty <connorfogarty26@gmail.com>
2019-01-21 13:50:45 -0600

    all of the Buttons work but no reset

--- app/src/main/java/com/example/robotics/deepspace/MainActivity.java
+++ app/src/main/java/com/example/robotics/deepspace/MainActivity.java
@@ -1,19 +1,50 @@
 package com.example.robotics.deepspace;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     Button BtnTP;
     Button BtnTN;
     Button BtnTnr;
     Button btnTPr;
-    int tn=0;
-    int tnr=0;
+    Button BtnMP;
+    Button BtnMN;
+    Button BtnMPr;
+    Button BtnMNr;
+    Button BtnBN;
+    Button BtnBP;
+    Button BtnBPr;
+    Button BtnBNr;
+    Button btnS1;
+    int tn = 0;
+    int tnr = 0;
+    int mn = 0;
+    int nmr = 0;
+    int BNr = 0;
+    int Bn = 0;
     TextView txtCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,16 +53,24 @@ public class MainActivity extends AppCompatActivity {
         BtnTN = findViewById(R.id.BtnTN);
         BtnTnr = findViewById(R.id.BtnTnr);
         btnTPr = findViewById(R.id.btnTPr);
-
+        BtnMP = findViewById(R.id.BtnMP);
+        BtnMN = findViewById(R.id.BtnMN);
+        BtnMNr = findViewById(R.id.BtnMNr);
+        BtnMPr = findViewById(R.id.BtnMPr);
+        BtnBP = findViewById(R.id.BtnBP);
+        BtnBN = findViewById(R.id.BtnBN);
+        BtnBNr = findViewById(R.id.BtnBNr);
+        BtnBPr = findViewById(R.id.BtnBPr);
+        btnS1 = findViewById(R.id.s1);
         BtnTP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 txtCount = findViewById(R.id.tn);
                 String countValue = txtCount.getText().toString();
                 int intCountValue = Integer.parseInt(countValue);
-                intCountValue = Math.min(intCountValue -0, 1);
+                intCountValue = Math.min(intCountValue - 0, 1);
                 intCountValue++;
-                tn=intCountValue;
+                tn = intCountValue;
                 txtCount.setText(String.valueOf(intCountValue));
             }
         });
@@ -42,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
                 String countValue = txtCount.getText().toString();
                 int intCountValue = Integer.parseInt(countValue);
                 intCountValue = Math.max(intCountValue - 1, 0);
-                tn=intCountValue;
+                tn = intCountValue;
                 txtCount.setText(String.valueOf(intCountValue));
             }
         });
@@ -53,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
                 String countValue = txtCount.getText().toString();
                 int intCountValue = Integer.parseInt(countValue);
                 intCountValue = Math.max(intCountValue - 1, 0);
-                tnr=intCountValue;
+                tnr = intCountValue;
                 txtCount.setText(String.valueOf(intCountValue));
             }
         });
@@ -63,11 +102,112 @@ public class MainActivity extends AppCompatActivity {
                 txtCount = findViewById(R.id.tnr);
                 String countValue = txtCount.getText().toString();
                 int intCountValue = Integer.parseInt(countValue);
-                intCountValue = Math.min(intCountValue -0, 1);
+                intCountValue = Math.min(intCountValue - 0, 1);
                 intCountValue++;
-                tnr=intCountValue;
+                tnr = intCountValue;
                 txtCount.setText(String.valueOf(intCountValue));
             }
         });
+        BtnMP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCount = findViewById(R.id.Mn);
+                String countValue = txtCount.getText().toString();
+                int intCountValue = Integer.parseInt(countValue);
+                intCountValue = Math.min(intCountValue - 0, 1);
+                intCountValue++;
+                mn = intCountValue;
+                txtCount.setText(String.valueOf(intCountValue));
+            }
+        });
+        BtnMN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCount = findViewById(R.id.Mn);
+                String countValue = txtCount.getText().toString();
+                int intCountValue = Integer.parseInt(countValue);
+                intCountValue = Math.max(intCountValue - 1, 0);
+                mn = intCountValue;
+                txtCount.setText(String.valueOf(intCountValue));
+            }
+        });
+        BtnMNr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCount = findViewById(R.id.nmr);
+                String countValue = txtCount.getText().toString();
+                int intCountValue = Integer.parseInt(countValue);
+                intCountValue = Math.max(intCountValue - 1, 0);
+                nmr = intCountValue;
+                txtCount.setText(String.valueOf(intCountValue));
+            }
+        });
+        BtnMPr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCount = findViewById(R.id.nmr);
+                String countValue = txtCount.getText().toString();
+                int intCountValue = Integer.parseInt(countValue);
+                intCountValue = Math.min(intCountValue - 0, 1);
+                intCountValue++;
+                nmr = intCountValue;
+                txtCount.setText(String.valueOf(intCountValue));
+            }
+
+
+        });
+        BtnBP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCount = findViewById(R.id.Bn);
+                String countValue = txtCount.getText().toString();
+                int intCountValue = Integer.parseInt(countValue);
+                intCountValue = Math.min(intCountValue - 0, 1);
+                intCountValue++;
+                nmr = intCountValue;
+                txtCount.setText(String.valueOf(intCountValue));
+            }
+        });
+        BtnBN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCount = findViewById(R.id.Bn);
+                String countValue = txtCount.getText().toString();
+                int intCountValue = Integer.parseInt(countValue);
+                intCountValue = Math.max(intCountValue - 1, 0);
+                Bn = intCountValue;
+                txtCount.setText(String.valueOf(intCountValue));
+            }
+
+        });
+        BtnBNr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCount = findViewById(R.id.BNr);
+                String countValue = txtCount.getText().toString();
+                int intCountValue = Integer.parseInt(countValue);
+                intCountValue = Math.max(intCountValue - 1, 0);
+                BNr = intCountValue;
+                txtCount.setText(String.valueOf(intCountValue));
+            }
+        });
+        BtnBPr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCount = findViewById(R.id.BNr);
+                String countValue = txtCount.getText().toString();
+                int intCountValue = Integer.parseInt(countValue);
+                intCountValue = Math.min(intCountValue - 0, 1);
+                intCountValue++;
+                BNr = intCountValue;
+                txtCount.setText(String.valueOf(intCountValue));
+
+            }
+
+
+
+        });
+
+
     }
 }
