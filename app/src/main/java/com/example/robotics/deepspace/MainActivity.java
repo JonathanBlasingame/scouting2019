48ff3d7a4c066a185050c57c8c2d5c67f565518d
Connor Fogarty <connorfogarty26@gmail.com>
2019-01-23 19:55:32 -0600

    all of the Buttons work and some clear

--- app/src/main/java/com/example/robotics/deepspace/MainActivity.java
+++ app/src/main/java/com/example/robotics/deepspace/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageButton;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -36,13 +37,21 @@ public class MainActivity extends AppCompatActivity {
     Button BtnBP;
     Button BtnBPr;
     Button BtnBNr;
-    Button btnS1;
+    AppCompatImageButton S1;
+
+
+    Intent mServiceIntent = null;
+
     int tn = 0;
     int tnr = 0;
     int mn = 0;
     int nmr = 0;
     int BNr = 0;
     int Bn = 0;
+    int teamNumber = 0;
+    String ScouterName = "";
+    int finalScore = 0;
+    String matchNumber= "";
     TextView txtCount;
 
     @Override
@@ -61,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         BtnBN = findViewById(R.id.BtnBN);
         BtnBNr = findViewById(R.id.BtnBNr);
         BtnBPr = findViewById(R.id.BtnBPr);
-        btnS1 = findViewById(R.id.s1);
+        S1 = findViewById(R.id.s1);
         BtnTP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
                 tn = intCountValue;
                 txtCount.setText(String.valueOf(intCountValue));
             }
+
         });
         BtnTN.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -203,11 +213,62 @@ public class MainActivity extends AppCompatActivity {
                 txtCount.setText(String.valueOf(intCountValue));
 
             }
-
-
-
         });
+        S1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File dir = new File(""); //BluetoothServerService.getDataDirectory();
+                ScouterName = ((TextView) findViewById(R.id.ScouterName)).getText().toString();
+                String notes = ((TextView) findViewById(R.id.notes)).getText().toString();
+                teamNumber = getIntValue(R.id.teamNumber, "Team Number");
+                finalScore = getIntValue(R.id.finalScore, "Final Score");
+                int match = getIntValue(R.id.matchNumber, "Match Number");
 
+                if (ScouterName == null || ScouterName.length() == 0 || match == 0 || teamNumber == 0 || finalScore == 0) {
+                    Toast.makeText(getApplicationContext(), "Please complete the form before saving", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Toast toast = Toast.makeText(getApplicationContext(), "Saving", Toast.LENGTH_SHORT);
+                toast.show();
+                resetapp();
+                try {
+
+                    Long myCurrentTimeMillis = System.currentTimeMillis();
+                    String timeMillis = Long.toString(myCurrentTimeMillis);
+                    File myFile = new File(dir, teamNumber + "_" + match + "_" + timeMillis + ".txt");
+                    FileOutputStream fOut = new FileOutputStream(myFile, true);
+                    PrintWriter myOutWriter = new PrintWriter(new OutputStreamWriter(fOut));
+                    myOutWriter.println("Scouter: " + ScouterName);
+                    myOutWriter.println("Team: " + teamNumber);
+                    myOutWriter.println("Timestamp: " + (new Date()).getTime());
+                    myOutWriter.println("Competition: ");
+                    myOutWriter.println("Match: " + match);
+
+                } catch (IOException e) {
+                    toast = Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT);
+                    Log.e("Exception", "File write failed: " + e.toString());
+                }
+            }
+        });
+    }
+    public void resetapp(){
+        ((CheckBox)findViewById(R.id.LH)).setChecked(false);
+        ((TextView)findViewById(R.id.matchNumber)).setText(""+(1+Integer.parseInt(((TextView)findViewById(R.id.matchNumber)).getText().toString())));
+        ((TextView)findViewById(R.id.teamNumber)).setText("");
+        ((TextView)findViewById(R.id.finalScore)).setText("");
+    }
+    private int getIntValue(int id, String name) {
+        String textValue = "" + ((TextView)findViewById(id)).getText();
+        try {
+            return Integer.parseInt(textValue);
+        } catch (NumberFormatException n) {
+            Toast.makeText(getApplicationContext(), name + " must have a numeric value", Toast.LENGTH_SHORT).show();
+            return 0;
+        }
 
     }
 }
+
+
+
+
